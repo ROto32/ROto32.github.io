838893fa109cc4786033ebd1c32a5cb5268db776
cer3web <otoontop@gmail.com>
2025-04-07 19:26:40 +0200

    Add files via upload

---
+++ spongebob/script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('inputText');
+const bubble = document.getElementById('bubbleText');
+
+input.addEventListener('input', () => {
+    bubble.textContent = input.value || "hi";
+});
