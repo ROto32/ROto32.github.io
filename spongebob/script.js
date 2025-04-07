cbcec7da83ea8763ce6033aee4ac581ac80eea80
cer3web <otoontop@gmail.com>
2025-04-07 19:11:33 +0200

    Delete spongebob directory

--- spongebob/script.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById('inputText');
-const bubble = document.getElementById('bubbleText');
-
-input.addEventListener('input', () => {
-    bubble.textContent = input.value || "Salut !";
-});
